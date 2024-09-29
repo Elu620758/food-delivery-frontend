904b2f350b3542ca443ec53cc276a3c14fe416ec
Elu620758 <elumalai620758@gmail.com>
2024-09-30 01:17:21 +0530

    header color changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable list of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <h2 style={{color:"#01007D"}}>Order your favourite food here</h2>
+                <p style={{color:"#01007D"}}>Choose from a diverse menu featuring a delectable list of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
             </div>
         </div>
     )
