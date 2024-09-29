2125fcb985d5a77e629134dc901ae7f234764cb2
Elu620758 <elumalai620758@gmail.com>
2024-09-30 00:06:44 +0530

    footer created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Delivering delicious meals right to your door</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
                 <img src={assets.linkedin_icon} alt="" />
             </div>
-        </div>
+        </div> 
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
+            <h2>EluRestaurant</h2>
             <ul>
                 <li>Home</li>
                 <li>About us</li>
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91-7904457579</li>
+                <li>contact@EluRestaurant.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © EluRestaurant.com - All Right Reserved.</p>
     </div>
   )
 }
