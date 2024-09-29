7703fcb221ea52494c27e14ec88e2a89fd11ee61
Elu620758 <elumalai620758@gmail.com>
2024-09-30 01:21:44 +0530

    last commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <div className='header-contents'>
                 <h2 style={{color:"#01007D"}}>Order your favourite food here</h2>
-                <p style={{color:"#01007D"}}>Choose from a diverse menu featuring a delectable list of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+                <p style={{color:"#2D2C32"}}>Choose from a diverse menu featuring a delectable list of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
             </div>
         </div>
     )
